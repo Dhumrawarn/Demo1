f8a9fee747929011fa6577b410b7400acebe91b4
Dhumrawarn <93244919+Dhumrawarn@users.noreply.github.com>
2021-10-27 20:44:33 +0530

    Update emo.java

--- TestRepository/src/DemoRepo/emo.java
+++ TestRepository/src/DemoRepo/emo.java
@@ -4,6 +4,10 @@ public class emo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 
 	}
 
